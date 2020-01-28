631ba7d03432c477b77b1e436e098a6bc9dc3759
Hasni <hichem.marih@hotmail.fr>
2020-01-28 11:37:12 +0000

    Première version de la vue principale

---
+++ header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <link rel = stylesheet type = text/css href = "css/style.css"/>
+    <title>Détecteur de Plagiat</title>
+</head>
+<header>
+    <h1>Application web pour détécter le plagiat </h1>
+</header>
+</html>
--- index.php
+++ index.php
@@ -1,12 +1,28 @@
-<html>
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="fr">
 <head>
-    <title>Test PHP</title>
+    <meta charset="UTF-8">
+    <link rel = stylesheet type = text/css href = "css/style.css"/>
 </head>
 <body>
-<?php
-$name = 'abdel zeler';
-echo '<p>bonjour '.$name.'</p>';
-echo "Hasni"; echo "Lmarhoum";
-?>
+    <?php
+        include "header.php";
+    ?>
+    <form method="POST" action="traitement.php">
+        <label for="story1" id ="zone1">Zone texte 1 :</label>
+        <textarea name="zone1"
+                  rows="5" cols="33">
+        </textarea>
+
+        <label for="story2" id = "zone2">Zone texte 2 :</label>
+        <textarea name="zone2"
+                  rows="5" cols="33">
+        </textarea>
+        <input type="submit" value="Valider">
+    </form>
+    <?php
+        include "footer.php";
+    ?>
 </body>
 </html>
