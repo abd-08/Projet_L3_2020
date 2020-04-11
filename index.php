9fc70746aee11befc380220d11fefb16ea7e05e1
Hasni <hichem.marih@hotmail.fr>
2020-04-11 21:46:10 +0200

    Modification du index.php (Que le style)

--- index.php
+++ index.php
@@ -2,27 +2,25 @@
 
 <body class="bg">
     <div class="container">
-        <br><br><br>
+        <br>
         <div class="row">
-            <div class="col-md-6 offset-md-3" style="margin: auto; background: white; padding: 20px; box-shadow: 10px 10px 5px #888">
+            <div class="col-md-6 offset-md-3">
                 <div class="panel-heading">
-                    <h2>Veuillez choisir une image : </h2>
+                    <h4>Veuillez choisir une image : </h4>
 
                 </div>
                 <hr>
                 <form action="Cloudvision.php" method="post" enctype="multipart/form-data">
                     <input type="file" name="image" accept="image/*" class="form-control">
                     <br>
-                    <button type="submit" style="border-radius: 0px;" formaction="Cloudvision.php" class="btn btn-lg btn-block btn-outline-success">Analyse Image Cloud Vision</button>
-                    <button type="submit" style="border-radius: 0px;" formaction="Tesseract.php" class="btn btn-lg btn-block btn-outline-success">Analyse Image TESSERACT</button>
+                    <button type="submit" formaction="Cloudvision.php" class="btn btn-lg btn-block btn-outline-dark">Analyse Image Cloud Vision</button>
+                    <button type="submit" formaction="Tesseract.php" class="btn btn-lg btn-block btn-outline-dark">Analyse Image TESSERACT</button>
                  </form>
                     <br/>
-                    <br/>
-                    <br/>
                     <form>
-                    <button type="submit" style="border-radius: 0px;" formaction="CompareText.php" class="btn btn-lg btn-block btn-outline-success">Comparer 2 textes</button>
-                    <button type="submit" style="border-radius: 0px;" formaction="CompareWeb.php" class="btn btn-lg btn-block btn-outline-success">Recherche plagiat internet</button>
-                    </form>
+                    <button type="submit" formaction="CompareText.php" class="btn btn-lg btn-block btn-outline-dark">Comparer 2 textes</button>
+                    <button type="submit" formaction="CompareWeb.php" class="btn btn-lg btn-block btn-outline-dark">Recherche plagiat internet</button>
+                    </form> <br/>
             </div>
         </div>
     </div>
